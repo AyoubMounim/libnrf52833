809a6a785347bfc11eac559cbeacc20e5c6b6867
Ayoub <mounim.ayoub@gmail.com>
2023-05-26 17:25:38 +0200

    added nvmc.c

---
+++ src/nvmc.c
@@ -0,0 +1,16 @@
+
+#include "nvmc.h"
+#include "macros.h"
+#include <stdint.h>
+
+
+#define NVMC_BASE 0x4001E000
+
+#define NVMC_READY      REG(uint32_t, NVMC_BASE, 0x400)
+#define NVMC_CONFIG     REG(uint32_t, NVMC_BASE, 0x504)
+#define NVMC_ERASEPAGE  REG(uint32_t, NVMC_BASE, 0x508)
+#define NVMC_ICACHECONF REG(uint32_t, NVMC_BASE, 0x540)
+
+#define NVMC_CONFIG_WEN 0
+#define NVMC_CONFIG_EEN 1
+#define NVMC_ICACHECONF_CACHEEN 0
